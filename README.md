bff0681469c7af4baea6ef788bc1dd4274d5a136
Cenachi <carloscenachi@protonmail.com>
2022-02-07 16:05:30 -0300

    firstCommit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Intro-WebRestful
+
+Um serviço que aceitará solicitações HTTP GET em http://localhost:8080/greeting.
+
+Ele responderá com uma representação JSON de uma saudação, como mostra a listagem a seguir:
+
+{"id":1,"content":"Hello, World!"}
+
+Você pode personalizar a saudação com um parâmetro opcional namena string de consulta, como mostra a lista a seguir:
+
+http://localhost:8080/greeting?name=User
+O namevalor do parâmetro substitui o valor padrão de Worlde é refletido na resposta, como mostra a listagem a seguir:
+
+{"id":1,"content":"Hello, User!"}
---
+++ src/main/java/com/restfulapi/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.restfulapi.controller;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.restfulapi.model.Greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private static final AtomicInteger counter = new AtomicInteger();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
